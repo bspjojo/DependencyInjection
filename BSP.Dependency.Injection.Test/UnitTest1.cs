c5119a1938c9ed87fd1381bea7ba6ded1cbf1fd6
bspjojo <thompsonjoseph0@gmail.com>
2020-08-16 18:30:24 +1200

    add test project

---
+++ BSP.Dependency.Injection.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace BSP.Dependency.Injection.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
